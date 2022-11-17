079973c66c41fc1c28ba87764ca3ffeb6ec6175f
JuanTasayco <juantasayco266@gmail.com>
2022-11-16 19:43:58 -0500

    v0.1

---
+++ src/app/lpm/lpm.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LpmRoutingModule } from './lpm-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LpmRoutingModule
+  ]
+})
+export class LpmModule { }
